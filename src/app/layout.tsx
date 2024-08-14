cf9ea9c6db1cef3fc0fe9daff8f8c9be0bbdcc15
Abhishek Mamgain <abhishekmmgn@gmail.com>
2024-08-14 23:32:03 +0530

    Temporarily removed auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "@/fonts/fonts";
 import { cn } from "@/lib/utils";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -18,26 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <UserProvider>
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            inter.className
-          )}
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
         >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Navbar />
-            <div className="mx-auto w-full px-5 sm:px-auto sm:container sm:max-w-6xl pt-16 sm:pt-20">
-              {children}
-            </div>
-          </ThemeProvider>
-        </body>
-      </UserProvider>
+          <Navbar />
+          <div className="mx-auto w-full px-5 sm:px-auto sm:container sm:max-w-6xl pt-16 sm:pt-20">
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,8 @@
-import { getSession } from "@auth0/nextjs-auth0";
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 import { ptSerif } from "@/fonts/fonts";
 import { cn } from "@/lib/utils";
 
 async function Navbar() {
-  const user = await getSession();
-  const picture = user?.user.picture;
-  const name = user?.user.picture.name;
   return (
     <div className="fixed top-0 inset-x-0 z-50 h-16 bg-[#fafafacc] bg-clip-padding backdrop-filter backdrop-blur-xl flex justify-between items-center sm:h-20 mx-auto w-full px-5 sm:px-auto sm:container sm:max-w-6xl dark:bg-[#050505dd]">
       <p
@@ -18,7 +14,7 @@ async function Navbar() {
         H
       </p>
       <Avatar className="h-8 w-8 md:h-9 md:w-9 lg:w-10 lg:h-10">
-        <AvatarImage src={picture} alt={name} />
+        <AvatarImage src="" alt="Profile picture" />
         <AvatarFallback className="text-sm+">A</AvatarFallback>
       </Avatar>
     </div>
